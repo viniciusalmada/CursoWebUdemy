0f2e07004691bd78cb512da665f928098b903560
archie <viniciusalmada10@gmail.com>
2019-08-13 01:07:03 +0000

    Aula 12 - Objects

---
+++ fundamentos/12objects.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4999.90
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+
+console.log(prod1)
+/* Objetos são conjuntos de pares 
+ * Chave-Valor */
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+}
+
+console.log(prod2)
